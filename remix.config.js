725e34f557b672d43540bee7d8dd012d24d34a5c
chima <chimajunior14@gmail.com>
2025-04-15 20:29:18 +0100

    new config

--- remix.config.js
+++ remix.config.js
@@ -1,9 +1,12 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  publicPath: "/build/",
   serverBuildTarget: "vercel",
-  serverModuleFormat: "esm",
-  ignoredRouteFiles: ["**/.*"]
+  ignoredRouteFiles: ["**/.*"],
 };
+
 
 
 // /** @type {import('@remix-run/dev').AppConfig} */
