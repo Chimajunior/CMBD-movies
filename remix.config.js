2c04f36393e62dd2485e609c3c3aaf5184de1906
chima <chimajunior14@gmail.com>
2025-04-15 20:01:42 +0100

    Final Vercel fix

--- remix.config.js
+++ remix.config.js
@@ -1,14 +1,22 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  appDirectory: "app",
-  assetsBuildDirectory: "public/build",
-  publicPath: "/build/",
-  serverBuildTarget: "vercel", 
-  ignoredRouteFiles: ["**/.*"],
-  future: {
-    v2_routeConvention: true,
-  },
+  serverBuildTarget: "vercel",
+  serverModuleFormat: "esm",
+  ignoredRouteFiles: ["**/.*"]
 };
+
+
+// /** @type {import('@remix-run/dev').AppConfig} */
+// export default {
+//   appDirectory: "app",
+//   assetsBuildDirectory: "public/build",
+//   publicPath: "/build/",
+//   serverBuildTarget: "vercel", 
+//   ignoredRouteFiles: ["**/.*"],
+//   future: {
+//     v2_routeConvention: true,
+//   },
+// };
 
 
 
