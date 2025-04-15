3556fc2e802496f063c8f8da9b2ae7a0027a4640
chima <chimajunior14@gmail.com>
2025-04-15 15:59:19 +0100

    second vercel commit

--- remix.config.js
+++ remix.config.js
@@ -9,3 +9,21 @@ export default {
     v2_routeConvention: true,
   },
 };
+
+
+
+// /** @type {import('@remix-run/dev').AppConfig} */
+// export default {
+//   future: {
+//     v2_routeConvention: true,
+//   },
+//   ignoredRouteFiles: ["**/.*"],
+
+//   appDirectory: "app",
+//   assetsBuildDirectory: "public/build",
+//   publicPath: "/build/",
+
+//   serverBuildDirectory: "api/build", 
+//   serverModuleFormat: "esm",         
+// };
+
